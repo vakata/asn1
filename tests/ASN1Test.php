714862864d0fe17c5327217e89f4ee5b1b9fd971
Ivan Bozhanov <git@vakata.com>
2016-02-20 15:15:17 +0200

    fixed test

--- tests/ASN1Test.php
+++ tests/ASN1Test.php
@@ -1,9 +1,66 @@
 <?php
 namespace vakata\asn1\test;
 
+use vakata\asn1\ASN1;
+
 class ASN1Test extends \PHPUnit_Framework_TestCase
 {
 	public function testEncodeDecode() {
-		$this->assertEquals(3, $data['reference2']);
+		$tsq = array(
+			'type' => ASN1::TYPE_SEQUENCE,
+			'children' => array(
+				'version' => array(
+					'type' => ASN1::TYPE_INTEGER,
+					'mapping' => array(1=>'v1','v2','v3')
+				),
+				'messageImprint' => array(
+					'type' => ASN1::TYPE_SEQUENCE,
+					'children' => array(
+						'hashAlgorithm' => array(
+							'type' => ASN1::TYPE_SEQUENCE,
+							'children' => array(
+								"algorithm" => array(
+									'type' => ASN1::TYPE_OBJECT_IDENTIFIER
+								),
+								'parameters' => array (
+									'type' => ASN1::TYPE_ANY,
+									'optional' => true
+								)
+							)
+						),
+						'hashedMessage' => array(
+							'type' => ASN1::TYPE_OCTET_STRING
+						)
+					)
+				),
+				'reqPolicy' => array (
+					'type' => ASN1::TYPE_OBJECT_IDENTIFIER,
+					'optional' => true
+				),
+				'nonce' => array (
+					'type' => ASN1::TYPE_INTEGER,
+					'optional' => true
+				),
+				'certReq' => array (
+					'type' => ASN1::TYPE_BOOLEAN,
+					'optional' => true
+				)
+			)
+		);
+
+		$src = array(
+			'version' => 'v1',
+			'messageImprint' => array (
+				'hashAlgorithm' => array("algorithm" => 'sha1', 'parameters' => null),
+				'hashedMessage' => base64_encode(sha1("asdf", true)),
+			),
+			'reqPolicy' => null,
+			'certReq' => true,
+			'nonce' => rand(0, PHP_INT_MAX)
+		);
+
+		$res = ASN1::encodeDER($src, $tsq);
+		$res = ASN1::decodeDER($res, $tsq);
+		$this->assertEquals($src, $res);
 	}
 }
