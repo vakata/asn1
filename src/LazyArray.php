33fd06bf84184e3a351357c593807e95df3825f0
Ivan Bozhanov <git@vakata.com>
2023-01-18 11:50:20 +0200

    updated signatures for php 8

--- src/LazyArray.php
+++ src/LazyArray.php
@@ -16,19 +16,19 @@ class LazyArray implements \ArrayAccess, \Iterator, \Countable
     {
         return $this[$k] ?? null;
     }
-    public function offsetExists($offset)
+    public function offsetExists($offset): bool
     {
         return isset($this->data[$offset]);
     }
-    public function offsetGet($offset)
+    public function offsetGet($offset): mixed
     {
         return call_user_func($this->processor, $this->data[$offset]);
     }
-    public function offsetSet($offset, $value)
+    public function offsetSet($offset, $value) : void
     {
         throw new \Exception('Not supported');
     }
-    public function offsetUnset($offset)
+    public function offsetUnset($offset) : void
     {
         throw new \Exception('Not supported');
     }
@@ -40,19 +40,19 @@ class LazyArray implements \ArrayAccess, \Iterator, \Countable
     {
         return key($this->data);
     }
-    public function next()
+    public function next(): void
     {
-        return next($this->data);
+        next($this->data);
     }
-    public function rewind()
+    public function rewind(): void
     {
-        return reset($this->data);
+        reset($this->data);
     }
-    public function valid()
+    public function valid(): bool
     {
         return key($this->data) !== null;
     }
-    public function count()
+    public function count(): int
     {
         return count($this->data);
     }
